ef6870989ef99cf3e8ffe98fda5a984c589f46c7
BetaJalba <cristianjalba4@gmail.com>
2025-09-26 09:28:43 +0200

    My Second committtitit!1!!!

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import com.google.gson.*;
+import com.google.gson.GsonBuilder;
+
+
 public class Main {
+    public static void main(String[] args) {
+        ApiClient apiClient = new ApiClient();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String rawJson = apiClient.fetchQuestions(5, "easy", "multiple");
+        JsonElement jsonElement = JsonParser.parseString(rawJson);
+        String prettyJson = gson.toJson(jsonElement);
+
+        System.out.println(prettyJson);
+    }
 }
